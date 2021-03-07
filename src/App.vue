84e17506a80fa56727a391753e02a7dfee0fe130
Marcelo Casamassa <marcelocasamassa@gmaile.com>
2021-03-07 10:53:46 -0300

    implement vu-router and components logic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Nav />
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from "./components/Nav.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav
   }
 }
 </script>
